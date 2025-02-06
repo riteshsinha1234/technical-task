a22dbdc1439b53a8e6a6b61a473659a18b215351
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2025-02-07 00:10:27 +0530

    deploy changes to pages

--- lib/components/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const white = Color(0xFFFFFFFF);
-const black = Color(0xFF000000);
-const blk55 = Color(0xFF555555);
-const gry20per = Color(0x33C4C4C4); //gery 20%
-const red = Color(0xFFFF2E00);
-const blk24 = Color(0xFF242424);
